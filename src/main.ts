ac6be81e86d4976f0c49e4c1daa39f6b177738ca
NorVirae <norbertmbafrank@gmail.com>
2024-06-28 08:52:18 +0100

    add package

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
   await app.listen(8080);
 }
 bootstrap();
